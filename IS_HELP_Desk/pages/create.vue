59719064dc03bfcc83d21bc3fc8f1a8070fa1cc7
Phapaza <76487561+Phapaza@users.noreply.github.com>
2023-08-20 17:10:07 +0700

    update
    - forgot_password Page
    - layout -> navbar
    - create home Page (little)

--- IS_HELP_Desk/pages/create.vue
+++ IS_HELP_Desk/pages/create.vue
@@ -1,40 +1,74 @@
 <template>
   <div class="container-fluid bg">
     <div class="d-flex justify-content-center align-items-center min-vh-100">
-      <b-card class="custom-card container-fluid" style="border-radius: 15px;">
+      <b-card class="custom-card container-fluid" style="border-radius: 15px">
         <div class="text-center">
-          <img src="~/assets/images/1.png" class="img-fluid custom-image">
+          <img src="~/assets/images/1.png" class="img-fluid custom-image" />
         </div>
         <b-form-group class="text-left" label="Username">
-          <b-form-input placeholder="Enter username or email" style="width: 400px;" v-model="username" />
+          <b-form-input
+            placeholder="Enter username or email"
+            style="width: 400px"
+            v-model="username"
+          />
         </b-form-group>
         <b-form-group class="text-left" label="First Name">
-          <b-form-input placeholder="Enter First Name" style="width: 400px;" v-model="firstName" />
+          <b-form-input
+            placeholder="Enter First Name"
+            style="width: 400px"
+            v-model="firstName"
+          />
         </b-form-group>
         <b-form-group class="text-left" label="Last Name">
-          <b-form-input placeholder="Enter Last Name" style="width: 400px;" v-model="lastName" />
+          <b-form-input
+            placeholder="Enter Last Name"
+            style="width: 400px"
+            v-model="lastName"
+          />
         </b-form-group>
         <b-form-group class="text-left" label="Status">
-          <b-form-select v-model="status" :options="statusOptions" style="width: 400px;" />
+          <b-form-select v-model="status" :options="statusOptions" style="width: 400px" />
         </b-form-group>
         <b-form-group class="text-left" label="ID Student" v-if="status === 'student'">
-          <b-form-input v-model="studentId" placeholder="Enter Student ID" style="width: 400px;" />
+          <b-form-input
+            v-model="studentId"
+            placeholder="Enter Student ID"
+            style="width: 400px"
+          />
         </b-form-group>
         <b-form-group class="text-left" label="Password">
-          <b-form-input v-model="password" type="password" placeholder="Enter password" style="width: 400px;" />
+          <b-form-input
+            v-model="password"
+            type="password"
+            placeholder="Enter password"
+            style="width: 400px"
+          />
           <div v-if="password.length > 0 && password.length < 8" class="text-danger">
             Password must be at least 8 characters.
           </div>
         </b-form-group>
         <b-form-group class="text-left" label="Confirm Password">
-          <b-form-input v-model="confirmPassword" type="password" placeholder="Enter Confirm Password"
-            style="width: 400px;" />
-          <div v-if="confirmPassword.length > 0 && password !== confirmPassword" class="text-danger">
+          <b-form-input
+            v-model="confirmPassword"
+            type="password"
+            placeholder="Enter Confirm Password"
+            style="width: 400px"
+          />
+          <div
+            v-if="confirmPassword.length > 0 && password !== confirmPassword"
+            class="text-danger"
+          >
             Passwords do not match.
           </div>
         </b-form-group>
-        <b-button class="mt-3 mb-4" type="submit" variant="success" style="width: 400px;" :disabled="!formValid"
-          @click="handleRegistration">
+        <b-button
+          class="mt-3 mb-4"
+          type="submit"
+          variant="success"
+          style="width: 400px"
+          :disabled="!formValid"
+          @click="handleRegistration"
+        >
           Register
         </b-button>
       </b-card>
@@ -46,16 +80,16 @@
 export default {
   data() {
     return {
-      username: '',
-      firstName: '',
-      lastName: '',
-      password: '',
-      confirmPassword: '',
-      status: 'student', // ค่าเริ่มต้นเป็น 'student'
-      studentId: '',
+      username: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirmPassword: "",
+      status: "student", // ค่าเริ่มต้นเป็น 'student'
+      studentId: "",
       statusOptions: [
-        { value: 'student', text: 'Student' },
-        { value: 'teacher', text: 'Teacher' },
+        { value: "student", text: "Student" },
+        { value: "teacher", text: "Teacher" },
       ],
     };
   },
@@ -64,7 +98,7 @@ export default {
       return this.password !== this.confirmPassword;
     },
     formValid() {
-      if (this.status === 'student') {
+      if (this.status === "student") {
         return (
           this.username &&
           this.firstName &&
@@ -91,7 +125,7 @@ export default {
       if (this.formValid) {
         // Perform registration logic here
         // For example, you can navigate to the home page
-        this.$router.push('/home');
+        this.$router.push("/");
       }
     },
   },
@@ -112,6 +146,6 @@ export default {
 }
 
 .bg {
-  background: linear-gradient(45deg, #84BFDD, #FFF7CF);
+  background: linear-gradient(45deg, #84bfdd, #fff7cf);
 }
 </style>
---
+++ IS_HELP_Desk/pages/forgotpassword.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container-fluid bg">
+    <div class="d-flex justify-content-center align-items-center min-vh-100">
+      <b-card class="custom-card container-fluid" style="border-radius: 15px;">
+        <div class="text-center mt-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="90"
+            height="90"
+            fill="currentColor"
+            class="bi bi-person-exclamation"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm.256 7a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"
+            />
+            <path
+              d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1.5a.5.5 0 0 0 1 0V11a.5.5 0 0 0-.5-.5Zm0 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z"
+            />
+          </svg>
+        </div>
+        <h2 class="text-center mb-2">
+          Forgot Password
+        </h2> 
+        <div class="container">
+          <div class="col-6 mx-auto">
+            <b-form-group class="text-center">
+              <h6>Please enter your Email</h6>
+            </b-form-group>
+            <b-form-input
+              placeholder="Enter your Email"
+              trim
+              type="email"
+              required
+            />
+          </div>
+        </div>
+        <div class="text-center">
+          <b-form-group class="mt-1" />
+          <div class="text-center">
+            <b-button
+              type="submit"
+              variant="success"
+            >
+              Submit
+            </b-button>
+          </div>
+        </div>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<style>
+.custom-card {
+  max-width: 470px;
+  margin: 0 auto;
+  border: 1px solid rgb(175, 175, 175);
+}
+body {
+  background: linear-gradient(45deg, #9ddeff, #fff7cf);
+}
+</style>
